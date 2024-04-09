9fe2ea98c7bb891e8c4b5e374acea1a1f971cc3c
newbie <42925459+mhl222@users.noreply.github.com>
2024-04-09 13:18:28 +0800

    feat(go-backen/server): 服务器广播用户消息

--- go-backen/server.go
+++ go-backen/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"sync"
 )
@@ -18,7 +19,7 @@ type Server struct {
 	Message chan string
 }
 
-// 创建server，工厂方法
+// NewServer 创建server，工厂方法
 func NewServer(ip string, port int) *Server {
 	server := &Server{
 		Ip:        ip,
@@ -29,7 +30,7 @@ func NewServer(ip string, port int) *Server {
 	return server
 }
 
-// 监听Message广播消息channel的goroutine，一旦有消息就发送给全部的在线User
+// ListenMessage 监听Message广播消息channel的goroutine，一旦有消息就发送给全部的在线User
 func (receiver *Server) ListenMessage() {
 	fmt.Println("消息广播协程启动")
 	for {
@@ -42,9 +43,10 @@ func (receiver *Server) ListenMessage() {
 	}
 }
 
-// 广播消息
+// BroadCast 广播消息
 func (receiver *Server) BroadCast(user *User, msg string) {
-	sendMsg := fmt.Sprintf("[%s] %s: %s\n", user.Addr, user.Name, msg)
+	//sendMsg := fmt.Sprintf("[%s] %s: %s\n", user.Addr, user.Name, msg)
+	sendMsg := "[" + user.Addr + "]" + " " + user.Name + ": " + msg
 	receiver.Message <- sendMsg
 }
 
@@ -57,18 +59,45 @@ func (receiver *Server) handler(con net.Conn) {
 	receiver.OnlineMap[user.Name] = user
 	receiver.mapLock.Unlock()
 	// 广播用户上线消息
-	receiver.BroadCast(user, "已上线")
+	receiver.BroadCast(user, "get online\r\n")
 
+	// 接受客户端消息
+	go func(conn net.Conn) {
+		buf := make([]byte, 4096)
+		for {
+			n, err := con.Read(buf)
+			if n == 0 {
+				receiver.BroadCast(user, "off  online\r\n")
+				delete(receiver.OnlineMap, user.Name)
+				err := con.Close()
+				if err != nil {
+					fmt.Println("链接关闭失败", err)
+					return
+				}
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("消息接收失败", err)
+				return
+			}
+
+			// 提取用户消息
+			msg := string(buf[:n-1])
+			// 用户消息广播
+			receiver.BroadCast(user, msg)
+		}
+	}(con)
 }
 
 func (receiver *Server) Start() {
 	// socket listen
-	listener, err := net.Listen("tcp", fmt.Sprintf("%receiver:%d", receiver.Ip, receiver.Port))
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", receiver.Ip, receiver.Port))
 	if err != nil {
 		fmt.Println("net.Listener err", err)
 		return
 	}
-	fmt.Printf("server run at %receiver:%d\n", receiver.Ip, receiver.Port)
+	fmt.Printf("server run at %s:%d\n", receiver.Ip, receiver.Port)
 
 	// close listen socket
 	defer func(listener net.Listener) {
